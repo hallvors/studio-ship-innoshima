7e5a6809bfdca0072992efdd7a799136bde63baf
Hallvord R. M. Steen <hallvord@minus.no>
2022-07-29 15:52:49 +0900

    rename teachers

--- sanity/deskStructure.js
+++ sanity/deskStructure.js
@@ -9,7 +9,7 @@ export default () =>
     .title('Studio SHIP')
     .items([
       S.documentListItem().id('global-config').schemaType('siteSettings').title('サイトの設定'),
-      S.documentTypeListItem('person').title('先生'),
+      S.documentTypeListItem('person').title('講師の紹介'),
       S.documentTypeListItem('activity').title('内容の紹介'),
       S.documentListItem().id('global-schedule').schemaType('schedule').title('タイムテーブル'),
       S.documentTypeListItem('page').title('ページ'),
