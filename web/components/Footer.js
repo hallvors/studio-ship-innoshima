bf26ef02352545e3d2bae508f29666ee802151b4
Hallvord R. M. Steen <hallvord@minus.no>
2022-08-01 14:11:33 +0900

    Temporary font tester code

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import Link from "next/link";
 import styles from "./Footer.module.css";
 import SimpleBlockContent from "./SimpleBlockContent";
 
 function Footer(props) {
   const { text } = props;
+  const [font, setFont] = useState(null);
+  const [fontReady, setFontReady] = useState(false);
+
   return (
-    <div className={styles.root}>
-      <div className={styles.text}>
-        <SimpleBlockContent content={text} />
+    <>
+      <div className={styles.root}>
+        <div className={styles.text}>
+          <SimpleBlockContent content={text} />
+        </div>
       </div>
-    </div>
+      <input
+        value={font}
+        onChange={(evt) => setFont(evt.target.value)}
+        onFocus={() => setFontReady(false)}
+        onBlur={(evt) => setFontReady(Boolean(font))}
+        placeholder="type font name here to test font"
+      />
+      {font && fontReady && (
+        <div>
+          <link
+            href={`https://fonts.googleapis.com/css2?family=${font}&display=swap`}
+            rel="stylesheet"
+          />
+          <style>
+            body {"{"}
+            font-family: {font};{"}"}
+          </style>
+        </div>
+      )}
+    </>
   );
 }
 
